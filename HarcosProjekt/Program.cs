0278bf44e5d577bfe0e54999f09de1e0a6f31f4c
bobiuszgithub <bcsaldi18@gmail.com>
2020-10-04 18:39:23 +0200

    2. A).

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,28 @@ namespace HarcosProjekt
     {
         static void Main(string[] args)
         {
+            //valamiért az iskolában egy másik diák nevével pusholta fel nekem
+            //ezért inkább egy új repositoryt csináltam 
+
+
+            List<Harcos> harcosok = new List<Harcos>();
+
+            StreamReader r = new StreamReader("harcosok 1.csv");
 
 
 
 
+           // harcosok.Add(new Harcos("Bob1", 1));
+           // harcosok.Add(new Harcos("Bob2", 2));
+          //  harcosok.Add(new Harcos("Bob3", 3));
+
+            foreach (Harcos item in harcosok)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
