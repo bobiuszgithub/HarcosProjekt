9cf2facd7c4ee6ba3c389d308262d109be2d3b84
bobiuszgithub <bcsaldi18@gmail.com>
2020-10-04 19:25:22 +0200

    3. d.)

--- HarcosProjekt/Program.cs
+++ HarcosProjekt/Program.cs
@@ -14,10 +14,10 @@ namespace HarcosProjekt
             //valamiért az iskolában egy másik diák nevével pusholta fel nekem
             //ezért inkább egy új repositoryt csináltam 
 
-
             List<Harcos> harcosok = new List<Harcos>();
 
             StreamReader r = new StreamReader("harcosok 1.csv");
+
 
             Console.WriteLine("adja meg a harcos nevét: ");
             string nev = Console.ReadLine();
@@ -46,6 +46,14 @@ namespace HarcosProjekt
 
             char valasz;
             int kor = 0;
+            int hanyadik;
+            List<int> sorszamok = new List<int>();
+            for (int i = 0; i < harcosok.Count; i++)
+            {
+                sorszamok.Add(i + 1);
+
+            }
+
             do
             {
                 kor++;
@@ -57,15 +65,33 @@ namespace HarcosProjekt
                     Console.WriteLine("{0}. {1}", i + 1, harcosok[i]);
 
                 }
-                Console.WriteLine("mit szeretne csinálni? \na.) Megküzdeni egy harcossal\nb.) Gyógyulni\n c.) Kilépni");
+                Console.WriteLine("\nmit szeretne csinálni? \na.) Megküzdeni egy harcossal\nb.) Gyógyulni\nc.) Kilépni");
                 valasz = Convert.ToChar(Console.ReadLine());
                 while (valasz != 'a' && valasz != 'b' && valasz != 'c')
                 {
-                    Console.WriteLine("mit szeretne csinálni? \na.) Megküzdeni egy harcossal\nb.) Gyógyulni\nc.) Kilépni");
+                    Console.WriteLine("\nmit szeretne csinálni? \na.) Megküzdeni egy harcossal\nb.) Gyógyulni\nc.) Kilépni");
                     valasz = Convert.ToChar(Console.ReadLine());
                 }
+
+
+                
                 if (valasz == 'a')
                 {
+                    Console.WriteLine("Hányadik harcossal szeretne megküzdeni a listából?");
+                    for (int i = 0; i < harcosok.Count; i++)
+                    {
+                        Console.Write("{0} ", i+1);
+                    }
+                    Console.WriteLine();
+                    hanyadik = Convert.ToInt32(Console.ReadLine());
+
+                    sorszamok.Remove(1);
+                    while (!sorszamok.Contains(hanyadik))
+                    {
+                        Console.WriteLine("Hányadik harcossal szeretne megküzdeni a listából?");
+                        hanyadik = Convert.ToInt32(Console.ReadLine());
+                    }
+                    harcosok[0].Megkuzd(harcosok[hanyadik-1]);
 
                 }
 
