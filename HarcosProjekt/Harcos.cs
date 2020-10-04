43df78be763ea3b9d96efc6c748f91390b76ae0d
bobiuszgithub <bcsaldi18@gmail.com>
2020-10-04 18:19:31 +0200

    Add project files.

---
+++ HarcosProjekt/Harcos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarcosProjekt
+{
+    class Harcos
+    {
+        private string nev;
+        private int szint;
+        private int tapsztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSebzes;
+
+
+
+
+
+
+    }
+}
